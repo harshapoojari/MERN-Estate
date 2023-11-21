d1e6d575e551c93296f5ac5684a76efff13044f9
harshapoojari <91778186+harshapoojari@users.noreply.github.com>
2023-11-21 13:22:48 +0530

    connect ro databse

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config()
+mongoose.connect(process.env.MONGO_PASS).then(()=>{
+    console.log("Connected to db");
+}).catch((err)=>{
+    console.log(err);
+})
  const app=express();
   app.listen(3000,()=>{
 console.log("Server is successfully running on 3000");
