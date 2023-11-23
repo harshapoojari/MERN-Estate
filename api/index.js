8ee96947858db7d694193db2398c022181f92cc1
harshapoojari <91778186+harshapoojari@users.noreply.github.com>
2023-11-23 11:10:33 +0530

    create signup router

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRouter from './route/user.route.js'
+
+
 dotenv.config()
 mongoose.connect(process.env.MONGO_PASS).then(()=>{
     console.log("Connected to db");
@@ -11,3 +14,4 @@ mongoose.connect(process.env.MONGO_PASS).then(()=>{
   app.listen(3000,()=>{
 console.log("Server is successfully running on 3000");
   })
+ app.use('/api/user/',userRouter)
